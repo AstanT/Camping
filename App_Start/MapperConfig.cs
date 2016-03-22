67540059dda449b50bd33c2579b614d8cda1bba1
AstanT <serkov.53542@gmail.com>
2016-03-22 13:50:27 +0200

    Update 1

---
+++ App_Start/MapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Camping.App_Start
+{
+    public class MapperConfig
+    {
+        public static void RegisterMapping()
+        {
+            
+        }
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using Camping.App_Start;
+using Camping.Core;
+using Camping.CW.Installers;
+using Camping.Data;
+using Castle.Windsor;
+using FluentValidation.Mvc;
+
+namespace Camping
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            FluentValidationModelValidatorProvider.Configure();
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            MapperConfig.RegisterMapping();
+            var container = new WindsorContainer().Install(new AdminInstaller());
+        }
+        protected void FormsAuthentication_OnAuthenticate(Object sender, FormsAuthenticationEventArgs e)
+        {
+            if (FormsAuthentication.CookiesSupported != true) return;
+            if (Request.Cookies[FormsAuthentication.FormsCookieName] == null) return;
+            try
+            {
+                //let us take out the username now                
+                string email = FormsAuthentication.Decrypt(Request.Cookies[FormsAuthentication.FormsCookieName].Value).Name;
+                string roles = string.Empty;
+
+
+
+                using (DataContext entities = new DataContext())
+                {
+                    User user = entities.User.SingleOrDefault(u => u.email == email);
+
+                    roles = user.roles.First(m => m.id == user.id).role.name;//user.Roles;
+                }
+                //Let us set the Pricipal with our user specific details
+                e.User = new System.Security.Principal.GenericPrincipal(
+                    new System.Security.Principal.GenericIdentity(email, "Forms"), roles.Split(';'));
+            }
+            catch (Exception)
+            {
+                //somehting went wrong
+            }
+        }
+    }
+}
